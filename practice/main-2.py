53add6a4abbdf314c7772c2a1b2b7fcc8d628a14
vimlesh <vimleshverma1998@gmail.com>
2026-06-23 23:38:37 +0530

    add tool call

---
+++ practice/main-2.py
@@ -0,0 +1,75 @@
+from typing import TypedDict, Annotated, List
+
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI 
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from settings import MODEL_API_KEY
+from tool_node import model_with_tools, tool_node
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=1, google_api_key=MODEL_API_KEY)
+
+class AgentState(TypedDict):
+    messages: Annotated[List[BaseMessage], add_messages]
+
+
+def should_continue(state: AgentState):
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    # If the LLM makes a tool call, then perform an action
+    if getattr(last_message, "tool_calls", None):
+        return "tool_node"
+
+    # Otherwise, we stop (reply to the user)
+    return END
+
+def llm_call(state: AgentState):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [
+            model_with_tools.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                    )
+                ]
+                + state["messages"]
+            )
+        ],
+        "llm_calls": state.get('llm_calls', 0) + 1
+    }
+
+
+# Build workflow
+agent_builder = StateGraph(AgentState)
+
+# Add nodes
+agent_builder.add_node("llm_call", llm_call)
+agent_builder.add_node("tool_node", tool_node)
+
+# Add edges to connect nodes
+agent_builder.add_edge(START, "llm_call")
+agent_builder.add_conditional_edges(
+    "llm_call",
+    should_continue,
+    ["tool_node", END]
+)
+agent_builder.add_edge("tool_node", "llm_call")
+
+# Compile the agent
+agent = agent_builder.compile()
+
+
+# from IPython.display import Image, display
+# display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
+
+
+# --- 5. Run the Graph ---
+inputs: AgentState = {"messages": [HumanMessage(content= "add 10 and 3 then add 1")]}
+final_state = agent.invoke(inputs)
+print(f"AI: {final_state['messages'][-1].content}")
+
---
+++ practice/tool_node.py
@@ -0,0 +1,68 @@
+from langchain_core.messages import ToolMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.tools import tool
+from settings import MODEL_API_KEY
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    google_api_key=MODEL_API_KEY,
+)
+
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    print(f"Multiplying {a} and {b}")
+    return a * b
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    
+    return a + b
+
+
+@tool
+def divide(a: int, b: int) -> float:
+    """Divide `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    print(f"Dividing {a} by {b}")
+    return a / b
+
+
+# Augment the LLM with tools
+tools = [add, multiply, divide]
+tools_by_name = {tool.name: tool for tool in tools}
+# print(tools_by_name)
+model_with_tools = model.bind_tools(tools)
+
+# tools_by_name dictionary will look like this:
+"""
+{'add': StructuredTool(name='add', description='Adds `a` and `b`.\n\n    Args:\n        a: First int\n        b: Second int', args_schema=<class 'langchain_core.utils.pydantic.add'>, func=<function add at 0x71e3028da340>), 'multiply': StructuredTool(name='multiply', description='Multiply `a` and `b`.\n\n    Args:\n        a: First int\n        b: Second int', args_schema=<class 'langchain_core.utils.pydantic.multiply'>, func=<function multiply at 0x71e302d58040>), 'divide': StructuredTool(name='divide', description='Divide `a` and `b`.\n\n    Args:\n        a: First int\n        b: Second int', args_schema=
+"""
+
+def tool_node(state: dict):
+    """Tool node that takes in a state with messages and calls the model with tools."""
+    print("--- Calling Tool Node ---")
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
